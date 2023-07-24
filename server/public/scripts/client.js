962d234526f7f7a785567de9a19f9808f57a1fa3
Forrest Nelson <forrestnnelson@gmail.com>
2023-07-24 14:35:01 -0500

    updated server side to take in new object and add to array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,23 +5,40 @@ $(document).ready(onReady);
 function onReady() {
     // console.log('jQuery ready');
 
-    // handler
     $('#addJokeButton').on('click', handleJokeButton)
 
 }
 
 function handleJokeButton(event) {
     event.preventDefault();
+    let jokeObject;
     let jokeTeller = $('#whoseJokeIn').val();
     let jokeSetUp = $('#questionIn').val();
     let jokePunchLine = $('#punchlineIn').val();
+    // I'm doing it this way so that an object is only created when all inputs were used for a complete object
     if (!jokeTeller || !jokeSetUp || !jokePunchLine) {
         alert('Please complete all inputs!')
     } else {
-        let jokeObject = {
-            whoseJoke: jokeTeller,
-            jokeQuestion: jokeSetUp,
-            punchLine: jokePunchLine
+        jokeObject = {
+            joker: jokeTeller,
+            question: jokeSetUp,
+            answer: jokePunchLine
         }
     }
+    console.log('new joke object is:', jokeObject);
+
+    $.ajax({
+        method: 'POST',
+        url: '/joke',
+        data: jokeObject
+    }).then((response) => {
+        // likely want to add a function here to run the GET when it's back
+        console.log('POST was successful, response:', response)
+    }).catch((error) => {
+        console.log('error caught:', error);
+        alert("ERROR!");
+    })
+
+
 }
+
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve back static files
+app.use(express.static('server/public'));
 
 let jokes = [
   {
@@ -34,8 +37,41 @@ let jokes = [
   }
 ];
 
-// serve back static files
-app.use(express.static('server/public'));
+app.post('/joke', (req, res) => {
+  res.send('req.body made it to the server!')
+  console.log('at joke, req.body:', req.body)
+  let newJoke;
+  let jokeTeller = req.body.joker;
+  let jokeSetUp = req.body.question;
+  let jokePunchLine = req.body.answer;
+  if (req.body == undefined) {
+    // here, me trying to not push an empty object onto the array
+  } else {
+    // new short-hand object assignment Key just taught us
+    newJoke = {
+      whoseJoke: jokeTeller,
+      jokeQuestion: jokeSetUp,
+      punchLine: jokePunchLine
+    };
+  }
+  // console.log("new joke bundled into new object at server:", newJoke);
+  jokes.push(newJoke);
+  console.log('jokes array is now:', jokes);
+
+  // res.sendStatus(201);
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
